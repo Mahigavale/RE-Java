be8d6228f49f8d2502b4b26e59b501ea79d1688a
Mahigavale <Maheshgavale07@gmail.com>
2024-12-12 17:51:33 +0530

    core

---
+++ CoreJava/src/datatypes/Basics.java
@@ -0,0 +1,56 @@
+package datatypes;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		
+		//keywords :52 keywords
+		//identifier : _&$ only allowed to start
+		//no number at the start of the given identifier.
+		
+		
+		//class names start with the capital words.
+		
+		//objects,me
+		
+		
+		
+		//  single line comment.
+		
+		/*
+		 * 
+		 * multiline comment.
+		 * 
+		 */
+		
+		/**
+		 * 
+		 *  Documentation comments .
+		 *  included in the .class files after compilation.
+		 * 
+		 * 
+		 */
+		
+		int _num1=45;
+		
+		int $num2=90;
+		
+		
+		System.out.println(_num1+ $num2);
+		
+		
+		
+		/**
+		 *the primitive data types are stored in the stack memory of the java. :faster;efficient
+		 *primitive data types can't use to call the methods.
+		 *they have the default values and can't be null.
+		 * 0,0.0, \u000=> default for the char.
+		 */
+		
+		
+		
+		
+
+	}
+
+}
