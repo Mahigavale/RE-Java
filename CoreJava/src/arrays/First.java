66bc2aae0e4b240c22a615d325d72d17aa8cbc0a
Mahigavale <Maheshgavale07@gmail.com>
2024-12-12 18:17:22 +0530

    One -d array

---
+++ CoreJava/src/arrays/First.java
@@ -0,0 +1,49 @@
+package arrays;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		/*
+		 *  One dimensional array.
+		 *  Two dimensional array.
+		 *  Multi-dimensional array.
+		 *  Jagged array You can say.
+		 */
+
+		
+		int [] numbers =new int[] {10,20,30,40,50};
+		
+		int [] numbers_2=new int[0];
+		
+		int [] numbers_3= {1,2,3,4,5,6,7,8,9,10};
+		
+		
+		
+		for(int i:numbers)
+		{
+			System.out.println(i);
+		}
+		
+		System.out.println("************************************************");
+		
+		for(int i:numbers_2)
+		{
+			System.out.println(i);
+		}
+		
+		
+		System.out.println("*************************************************");
+		
+		for(int i:numbers_3)
+		{
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+	}
+
+}
