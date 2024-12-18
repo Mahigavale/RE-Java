3b9c8cf5c17b1515d9b68bede361d0b01b9db6b7
Mahigavale <Maheshgavale07@gmail.com>
2024-12-18 23:13:24 +0530

    java custom exception

--- Core_java_2/src/demor/Throws_demo.java
+++ Core_java_2/src/demor/Throws_demo.java
@@ -3,8 +3,37 @@ package demor;
 public class Throws_demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Throwre re=new Throwre();
 
+		try {
+			
+		re.addnums(123,234);
+		}
+		catch(Customexception c)
+		{
+			System.out.println(c.getMessage());
+		}
+		finally {
+			
+		}
 	}
 
+}
+
+class Throwre
+{
+	public void addnums(int i, int j) throws  Customexception
+	{
+		System.out.println(i+j);
+	}
+}
+
+class Customexception extends Exception
+{
+	
+	public Customexception()
+	{
+		super("custom exception hits here !");
+	}
 }
