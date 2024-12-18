838b0207053329e7512f357b1e17cc0cf06f101f
Mahigavale <Maheshgavale07@gmail.com>
2024-12-18 16:13:20 +0530

    Inner class in java

---
+++ CoreJava/src/Misc/Innerclass.java
@@ -0,0 +1,33 @@
+package Misc;
+
+public class Innerclass {
+
+	public static void main(String[] args) {
+		
+		outer out=new outer();
+		
+		outer.Inner inn=out.new Inner();
+		
+		
+		out.Show();
+		inn.innershow();
+		
+	}
+}
+
+
+class outer
+{
+	
+	public void Show()
+	{
+		System.out.println("show method from the outer class.");
+	}
+	class Inner{
+		
+		public void innershow()
+		{
+			System.out.println("show from inner class.");
+		}
+	}
+}
