c4bd68e7b4111aa01ff8e1dc39c6255647ffda89
Mahigavale <Maheshgavale07@gmail.com>
2025-03-02 23:58:09 +0530

    Reflection

---
+++ Objectclass/src/practise/Object_one.java
@@ -0,0 +1,72 @@
+package practise;
+
+
+
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Object_one {
+
+	public static void main(String[] args) {
+		
+		Player one =new Player(1,"SRT");
+		Player two=new Player(1,"SRT");
+		
+		System.out.println(one.equals(two));
+
+		System.out.println(one.hashCode());
+		System.out.println(two.hashCode());
+		
+		
+		Set<Player> set=new LinkedHashSet();
+		
+		set.add(one);
+		set.add(two);
+		
+		
+		
+	}
+
+}
+
+class Player extends Object{
+	
+	int id;
+	String name;
+	
+	
+	public Player(int id, String name)
+	{
+		 this.id=id;
+		 this.name=name;
+	}
+	
+	
+	
+	public int hashCode()
+	{
+		return 1000+this.id;
+	}
+	
+	
+	
+	public boolean equals(Object obj)
+	{
+		if(obj.getClass() ==null)
+		{
+			return false;
+		}
+		
+		if(this.getClass() != obj.getClass() )
+		{
+			return false;
+		}
+		
+		Player p=(Player)obj;
+		return (this.id==p.id && this.name.equals(p.name));
+		
+	}
+	
+	
+}
